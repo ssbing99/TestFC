5e3296fe9cd5a4a6da9e038db5bd29dc6247733e
Philip Goh <philip.goh05@gmail.com>
2016-10-26 00:20:55 +0800

    pls push github b0sssss

--- app/src/main/java/my/com/taruc/fitnesscompanion/UI/ChartPage.java
+++ app/src/main/java/my/com/taruc/fitnesscompanion/UI/ChartPage.java
@@ -14,34 +14,32 @@ import my.com.taruc.fitnesscompanion.R;
 
 public class ChartPage extends ActionBarActivity {
 
-    @BindView(R.id.webViewChart)
-    WebView webViewChart;
+    private WebView webViewChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart_page);
-        ButterKnife.bind(this);
 
-        webViewChart.loadUrl("http://tarucfit.pe.hu/ServerRequest/ViewChart.html");
-        webViewChart.setWebViewClient(new WebViewChartClient());
-        WebSettings webSettings = webViewChart.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        webViewChart = (WebView) findViewById(R.id.webViewChart);
+
+        webViewChart.setWebViewClient(new MyWebViewClient());
+
+        String url = "http://tarucfit.pe.hu/ServerRequest/ViewChart.html";
+        webViewChart.getSettings().setJavaScriptEnabled(true);
+        webViewChart.getSettings().setUseWideViewPort(true);
+        webViewChart.getSettings().setLoadWithOverviewMode(true);
+        webViewChart.loadUrl(url);
     }
 
-    private class WebViewChartClient extends WebViewClient {
-
+    private class MyWebViewClient extends WebViewClient{
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().equals("http://tarucfit.pe.hu/ServerRequest/ViewChart.html")) {
-                return false;
-            }
-
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-            startActivity(intent);
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
             return true;
         }
     }
+
 
 
 }
